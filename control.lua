c3b40ddd97be06c0d0e17fc3ecc39697bcb87ae9
lecht <lecht42@gmail.com>
2022-06-04 17:22:18 +0300

    Second commit

--- control.lua
+++ control.lua
@@ -1,6 +1,26 @@
-script.on_event("ruler-start", function(event)
+from = nil
+
+script.on_event("ruler-hotkey", function(event)
     local player = game.get_player(event.player_index)
-    local cursor = event.cursor_position
-    game.print(player.color)
-    rendering.draw_line({surface = player.character.surface, from = {cursor.x, cursor.y}, to = {100, 100}, color = player.color, width = 2})
+    local stack = player.cursor_stack
+
+    stack.set_stack({name="ruler-selection", count=1})
+end)
+
+script.on_event("mouse-memorizer", function(event)
+    from = event.cursor_position
+end)
+
+script.on_event(defines.events.on_player_selected_area, function(event)
+    local player = game.get_player(event.player_index)
+    local area = event.area
+    local to = (() => {
+        return "test"
+      })()
+
+    game.print(to)
+
+    if(event.item == "ruler-selection") then
+        rendering.draw_line({surface = player.character.surface, from = from, to = area.left_top, color = player.color, width = 3})   
+    end
 end)
--- ruler.lua
+++ ruler.lua
@@ -1,8 +1,31 @@
-local rulerStartHotkey = {
+local invisible = {r=0, g=0, b=0, a=0}
+
+local rulerHotkey = {
     type = "custom-input",
-    name = "ruler-start",
+    name = "ruler-hotkey",
     key_sequence = "SHIFT + C",
     consuming = "none"
 }
 
-data:extend{rulerStartHotkey}
+local mouseMemorizer = {
+    type = "custom-input",
+    name = "mouse-memorizer",
+    key_sequence = "mouse-button-1",
+    consuming = "none"
+}
+
+local ruler = {
+    type = "selection-tool",
+    name = "ruler-selection",
+    stack_size = 1,
+    icon = "__base__/graphics/icons/fluid/barreling/barrel-empty.png",
+    icon_size = 32,
+    selection_color = invisible,
+    alt_selection_color = invisible,
+    selection_mode = "nothing",
+    alt_selection_mode = "any-tile",
+    selection_cursor_box_type = "pair",
+    alt_selection_cursor_box_type = "pair",
+}
+
+data:extend{ruler, rulerHotkey, mouseMemorizer}
